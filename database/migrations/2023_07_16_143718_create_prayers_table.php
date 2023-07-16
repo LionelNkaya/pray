0599e064b490ea7e8721b831b6544585718fcc6b
Lionel Nkaya <lionel.nkaya@gmail.com>
2023-07-16 09:11:45 -0700

    improving the prayers table

--- database/migrations/2023_07_16_143718_create_prayers_table.php
+++ database/migrations/2023_07_16_143718_create_prayers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('prayers', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained('users');
             $table->text('content');
             $table->boolean('is_answered')->default(false);
             $table->timestamps();
