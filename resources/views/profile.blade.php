2b2dcffcf2d31ff97c58a656b63b3697fcd9780f
Lionel Nkaya <lionel.nkaya@gmail.com>
2023-12-09 22:17:30 -0800

    new attempt to display modal

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,27 +3,16 @@
 @section('content')
 
 <!-- Modal -->
-<div class="modal fade" id="profileModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalLabel">Profile Page</h5>
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <a href="{{ route('home') }}"> 
-              <span aria-hidden="true">&times;</span>
-            </a>
-          </button>
-        </div>
-        <div class="modal-body">
-            <p>{{ Auth::user()->name }}</p>  
-            <p>{{ Auth::user()->email }}</p>
-            <p>Joined: {{ Auth::user()->created_at->format('F Y') }}</p>
-            <p>Number of prayers recorded: {{ $userPrayerCount }}</p>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal"><a href="{{ route('home') }}">Close</a></button>
-        </div>
-      </div>
-    </div>
-  </div>
+<p>Test</p>
+  <x-modal id="vertically-centered" title="Vertically centered" :centered="true">
+    <x-slot name="body">
+      <p>{{ Auth::user()->name }}</p>  
+      <p>{{ Auth::user()->email }}</p>
+      <p>Joined: {{ Auth::user()->created_at->format('F Y') }}</p>
+      <p>Number of prayers recorded: {{ $userPrayerCount }}</p> 
+    </x-slot>
+    <x-slot name="footer">
+      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+    </x-slot>
+  </x-modal>
   @endsection
