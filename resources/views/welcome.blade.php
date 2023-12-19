bdbc4ffb8343b126769aa756420f7682270ec9f0
Lionel Nkaya <lionel.nkaya@gmail.com>
2023-12-18 22:15:00 -0800

    completed landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -48,7 +48,6 @@
             </div>
         </div>
 
-        <!-- Button trigger modal -->
 
   <!-- Modal -->
   <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
